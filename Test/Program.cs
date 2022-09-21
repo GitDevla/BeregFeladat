c5996b67bf017538f93ab180e8e658e6852208d0
Devla <62154973+GitDevla@users.noreply.github.com>
2022-09-21 12:48:41 +0200

    2.0

--- Test/Program.cs
+++ Test/Program.cs
@@ -3,7 +3,10 @@
 namespace Test {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            Console.Write("Első szám: ");
+            var elso = int.Parse(Console.ReadLine());
+            Console.Write("Második szám: ");
+            var masodik = int.Parse(Console.ReadLine());
         }
     }
 }
