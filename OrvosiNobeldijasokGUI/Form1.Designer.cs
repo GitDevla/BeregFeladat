bdcde587a607f901ee9fc323e61992bed07bccc6
GitDevla <pataki.david.ferenc@gmail.com>
2022-11-09 17:36:32 +0100

    Create visuals

--- OrvosiNobeldijasokGUI/Form1.Designer.cs
+++ OrvosiNobeldijasokGUI/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿
 namespace OrvosiNobeldijasokGUI {
-    partial class Form1 {
+    partial class Window {
         /// <summary>
         /// Required designer variable.
         /// </summary>
@@ -24,20 +24,126 @@ namespace OrvosiNobeldijasokGUI {
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button_save = new System.Windows.Forms.Button();
+            this.textBox_year = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox_name = new System.Windows.Forms.TextBox();
+            this.textBox_birthDate = new System.Windows.Forms.TextBox();
+            this.textBox_country = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // Form1
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 28);
+            this.label1.Margin = new System.Windows.Forms.Padding(3, 0, 3, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Év:";
+            // 
+            // button_save
+            // 
+            this.button_save.Location = new System.Drawing.Point(74, 178);
+            this.button_save.Name = "button_save";
+            this.button_save.Size = new System.Drawing.Size(112, 23);
+            this.button_save.TabIndex = 1;
+            this.button_save.Text = "Adatok mentése";
+            this.button_save.UseVisualStyleBackColor = true;
+            // 
+            // textBox_year
+            // 
+            this.textBox_year.Location = new System.Drawing.Point(89, 25);
+            this.textBox_year.Name = "textBox_year";
+            this.textBox_year.Size = new System.Drawing.Size(51, 20);
+            this.textBox_year.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 61);
+            this.label2.Margin = new System.Windows.Forms.Padding(3, 0, 3, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Név:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 94);
+            this.label3.Margin = new System.Windows.Forms.Padding(3, 0, 3, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Sz/H:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(30, 127);
+            this.label4.Margin = new System.Windows.Forms.Padding(3, 0, 3, 20);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Ország:";
+            // 
+            // textBox_name
+            // 
+            this.textBox_name.Location = new System.Drawing.Point(89, 58);
+            this.textBox_name.Name = "textBox_name";
+            this.textBox_name.Size = new System.Drawing.Size(135, 20);
+            this.textBox_name.TabIndex = 6;
+            // 
+            // textBox_birthDate
+            // 
+            this.textBox_birthDate.Location = new System.Drawing.Point(89, 91);
+            this.textBox_birthDate.Name = "textBox_birthDate";
+            this.textBox_birthDate.Size = new System.Drawing.Size(87, 20);
+            this.textBox_birthDate.TabIndex = 7;
+            // 
+            // textBox_country
+            // 
+            this.textBox_country.Location = new System.Drawing.Point(89, 124);
+            this.textBox_country.Name = "textBox_country";
+            this.textBox_country.Size = new System.Drawing.Size(51, 20);
+            this.textBox_country.TabIndex = 8;
+            // 
+            // Window
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(262, 213);
+            this.Controls.Add(this.textBox_country);
+            this.Controls.Add(this.textBox_birthDate);
+            this.Controls.Add(this.textBox_name);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox_year);
+            this.Controls.Add(this.button_save);
+            this.Controls.Add(this.label1);
+            this.Name = "Window";
+            this.Text = "Orvosi Nobel-díjasok";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button_save;
+        private System.Windows.Forms.TextBox textBox_year;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox_name;
+        private System.Windows.Forms.TextBox textBox_birthDate;
+        private System.Windows.Forms.TextBox textBox_country;
     }
 }
 
