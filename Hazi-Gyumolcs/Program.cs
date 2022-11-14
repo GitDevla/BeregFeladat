857522ca4e2d9c29ad2ebc99d428534da0f5f79d
GitDevla <pataki.david.ferenc@gmail.com>
2022-11-14 16:23:55 +0100

    Create visuals

--- Hazi-Gyumolcs/Program.cs
+++ Hazi-Gyumolcs/Program.cs
@@ -13,7 +13,7 @@ namespace Hazi_Gyumolcs {
         static void Main() {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Window());
         }
     }
 }
