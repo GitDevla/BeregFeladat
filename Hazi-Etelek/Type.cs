4d03201ebda49f054f56a74e8020670f403fc6ee
GitDevla <pataki.david.ferenc@gmail.com>
2022-12-05 19:18:58 +0100

    Add Read operation to Types

--- Hazi-Etelek/Type.cs
+++ Hazi-Etelek/Type.cs
@@ -1,4 +1,7 @@
-﻿namespace Hazi_Etelek {
+﻿using DLib;
+using System.Collections.Generic;
+
+namespace Hazi_Etelek {
     class Type {
         public readonly int Id;
         public readonly string Name;
@@ -6,6 +9,21 @@
         public Type(int id, string name) {
             Id = id;
             Name = name;
+        }
+
+        static public List<Type> DBRead(SQL db) {
+            var res = db.Query("Select * FROM `kategoria`;");
+            var list = new List<Type>();
+            foreach (var item in res.Values) {
+                var id = int.Parse(item[0]);
+                var name = item[1];
+                list.Add(new Type(id, name));
+            }
+            return list;
+        }
+
+        public override string ToString() {
+            return Name;
         }
     }
 }
