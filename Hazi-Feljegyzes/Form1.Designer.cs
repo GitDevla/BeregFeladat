8df08b92f5e4705cb3b7046a052c28dad8cacd97
cisco <cisco@B101-20>
2022-12-12 09:47:46 +0100

    Add project files.

---
+++ Hazi-Feljegyzes/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace Hazi_Feljegyzes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_name = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dateTimePicker_date = new System.Windows.Forms.DateTimePicker();
+            this.richTextBox = new System.Windows.Forms.RichTextBox();
+            this.button_open = new System.Windows.Forms.Button();
+            this.button_save = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(114, 36);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Név:";
+            // 
+            // textBox_name
+            // 
+            this.textBox_name.Location = new System.Drawing.Point(186, 33);
+            this.textBox_name.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.textBox_name.Name = "textBox_name";
+            this.textBox_name.Size = new System.Drawing.Size(172, 34);
+            this.textBox_name.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(442, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 29);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Dátum:";
+            // 
+            // dateTimePicker_date
+            // 
+            this.dateTimePicker_date.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker_date.Location = new System.Drawing.Point(536, 31);
+            this.dateTimePicker_date.Name = "dateTimePicker_date";
+            this.dateTimePicker_date.Size = new System.Drawing.Size(166, 34);
+            this.dateTimePicker_date.TabIndex = 3;
+            // 
+            // richTextBox
+            // 
+            this.richTextBox.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.richTextBox.Location = new System.Drawing.Point(0, 136);
+            this.richTextBox.Name = "richTextBox";
+            this.richTextBox.Size = new System.Drawing.Size(886, 255);
+            this.richTextBox.TabIndex = 4;
+            this.richTextBox.Text = "";
+            // 
+            // button_open
+            // 
+            this.button_open.Location = new System.Drawing.Point(256, 91);
+            this.button_open.Name = "button_open";
+            this.button_open.Size = new System.Drawing.Size(123, 39);
+            this.button_open.TabIndex = 5;
+            this.button_open.Text = "Megnyit";
+            this.button_open.UseVisualStyleBackColor = true;
+            this.button_open.Click += new System.EventHandler(this.button_open_Click);
+            // 
+            // button_save
+            // 
+            this.button_save.Location = new System.Drawing.Point(468, 91);
+            this.button_save.Name = "button_save";
+            this.button_save.Size = new System.Drawing.Size(123, 39);
+            this.button_save.TabIndex = 6;
+            this.button_save.Text = "Mentés";
+            this.button_save.UseVisualStyleBackColor = true;
+            this.button_save.Click += new System.EventHandler(this.button_save_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog1";
+            this.openFileDialog.Filter = "Szöveges file *.txt|*.txt|Pontosvessző tábla *.csv|*.csv";
+            // 
+            // saveFileDialog
+            // 
+            this.saveFileDialog.Filter = "Szöveges file *.txt|*.txt|Pontosvessző tábla *.csv|*.csv";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(886, 391);
+            this.Controls.Add(this.button_save);
+            this.Controls.Add(this.button_open);
+            this.Controls.Add(this.richTextBox);
+            this.Controls.Add(this.dateTimePicker_date);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox_name);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_name;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker_date;
+        private System.Windows.Forms.RichTextBox richTextBox;
+        private System.Windows.Forms.Button button_open;
+        private System.Windows.Forms.Button button_save;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+    }
+}
+
---
+++ Hazi-Feljegyzes/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hazi_Feljegyzes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_open_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                openFileDialog.AddExtension = true;
+                var path = openFileDialog.FileName;
+                if(string.IsNullOrWhiteSpace(path)) return;
+                textBox_name.Text = openFileDialog.SafeFileName;
+                richTextBox.Text = File.ReadAllText(path);
+            }
+        }
+
+        private void button_save_Click(object sender, EventArgs e)
+        {
+            saveFileDialog.FileName = textBox_name.Text;
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+
+                var date = dateTimePicker_date.Value;
+                var path = saveFileDialog.FileName;
+                File.WriteAllText(path, $"====={date}=====\n");
+                File.AppendAllText(path,richTextBox.Text);
+                richTextBox.Text = File.ReadAllText(path);
+            }
+        }
+    }
+}
