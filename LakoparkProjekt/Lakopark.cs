ad41fdb3c67d4e75a106bd0696610436472c0d3b
asd <asd>
2023-01-18 13:26:42 +0100

    Add lakopark class

---
+++ LakoparkProjekt/Lakopark.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LakoparkProjekt
+{
+    internal class Lakopark
+    {
+        private int[,] hazak;
+        private int maxHazSzam;
+        private string nev;
+        private int utcakSzama;
+
+        public Lakopark(string nev,int utcakSzama,int maxHazSzam, int[,] hazak)
+        {
+            this.hazak = hazak;
+            this.maxHazSzam = maxHazSzam;
+            this.nev = nev;
+            this.utcakSzama = utcakSzama;
+        }
+
+        public int[,] Hazak { get { return hazak; } }
+        public int MaxHazSzam { get { return maxHazSzam; } }
+        public string Nev { get { return nev; } }
+        public int UtcakSzama { get { return utcakSzama; } }
+
+        
+    }
+}
