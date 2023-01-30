6589b1ca03b7187d189a42f7f68c2d0ef797a76a
GitDevla <pataki.david.ferenc@gmail.com>
2023-01-30 18:27:42 +0100

    Done

---
+++ Hazi-Math/Matrix.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Hazi_Math {
+    static class Matrix {
+        public static int[,] Transform(int[,] a, int[,] b,Func<int,int,int> f) {
+            if (a.GetLength(0) != b.GetLength(0)
+                || a.GetLength(1) != b.GetLength(1)) throw new FormatException();
+
+            int height = a.GetLength(0), width = a.GetLength(1);
+            int[,] sumMatrix = new int[height, width];
+            for (int y = 0; y < height; y++)
+                for (int x = 0; x < width; x++)
+                    sumMatrix[x, y] = f(a[x, y],b[x, y]);
+
+            return sumMatrix;
+        }
+
+        public static int[,] Sum(int[,] a, int[,] b) {
+            return Transform(a, b, (a, b) => a + b);
+        }
+
+        public static int[,] Sub(int[,] a, int[,] b) {
+            return Transform(a, b, (a, b) => a - b);
+        }
+
+        public static int[,] Scalar(int[,] a, int[,] b) {
+            return Transform(a, b, (a, b) => a * b);
+        }
+    }
+}
--- Hazi-Math/Program.cs
+++ Hazi-Math/Program.cs
@@ -2,8 +2,59 @@
 
 namespace Hazi_Math {
     class Program {
+        static readonly (string op, Func<int[,], int[,], int[,]> fun)[] operations = {
+            ("Összeadás", Matrix.Sum),
+            ("Kivonás", Matrix.Sub),
+            ("Skaláris Szorzás", Matrix.Scalar),
+        };
+
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Első mátrix: ");
+            var a = ReadMatrix();
+            Console.WriteLine("\nMásodik mátrix: ");
+            var b = ReadMatrix();
+
+            Console.WriteLine("\nKérem válasszon egy Mátrix operációt: ");
+            for (int i = 0; i < operations.Length; i++) {
+                var (op, _) = operations[i];
+                Console.WriteLine($"\t {i+1}. {op}");
+            }
+            Console.Write("Válasz: ");
+            var input = int.Parse(Console.ReadLine());
+
+            if (a.GetLength(0) != b.GetLength(0)
+                || a.GetLength(1) != b.GetLength(1)) {
+                Console.WriteLine("A mátrixok nem megegyező méretűek");
+                return;
+            }
+            var res = operations[input - 1].fun(a, b);
+            Console.WriteLine("\nKapott mátrix az operáció elvégzése után:");
+            DrawMatrix(res);
+        }
+
+        static int[,] ReadMatrix() {
+            Console.Write("Kérem a mátrix oszlopainak számát: ");
+            int height = int.Parse(Console.ReadLine());
+            Console.Write("Kérem a mátrix sorainak számát: ");
+            int width = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[height, width];
+            for (int y = 0; y < height; y++) {
+                for (int x = 0; x < width; x++) {
+                    Console.Write($"Kérem a mátrix [i:{y+1},j:{x+1}] elemét: ");
+                    matrix[y, x] = int.Parse(Console.ReadLine());
+                }
+            }
+            return matrix;
+        }
+
+        static void DrawMatrix(int[,] matrix) {
+            int height = matrix.GetLength(0), width = matrix.GetLength(1);
+            for (int y = 0; y < height; y++) {
+                for (int x = 0; x < width; x++)
+                    Console.Write("{0}\t", matrix[x, y]);
+                Console.WriteLine();
+            }
         }
     }
 }
