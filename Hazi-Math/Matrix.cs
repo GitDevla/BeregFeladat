c8930fe9f6da1ffbcb8f2f3d50af555b15331b3e
GitDevla <pataki.david.ferenc@gmail.com>
2023-01-31 17:37:58 +0100

    Add dot product

--- Hazi-Math/Matrix.cs
+++ Hazi-Math/Matrix.cs
@@ -3,16 +3,15 @@
 namespace Hazi_Math {
     static class Matrix {
         public static int[,] Transform(int[,] a, int[,] b,Func<int,int,int> f) {
-            if (a.GetLength(0) != b.GetLength(0)
-                || a.GetLength(1) != b.GetLength(1)) throw new FormatException();
+            if (!IsSameSize(a,b)) throw new FormatException();
 
             int height = a.GetLength(0), width = a.GetLength(1);
-            int[,] sumMatrix = new int[height, width];
+            int[,] outMatrix = new int[height, width];
             for (int y = 0; y < height; y++)
                 for (int x = 0; x < width; x++)
-                    sumMatrix[x, y] = f(a[x, y],b[x, y]);
+                    outMatrix[x, y] = f(a[x, y],b[x, y]);
 
-            return sumMatrix;
+            return outMatrix;
         }
 
         public static int[,] Sum(int[,] a, int[,] b) {
@@ -25,6 +24,36 @@ namespace Hazi_Math {
 
         public static int[,] Scalar(int[,] a, int[,] b) {
             return Transform(a, b, (a, b) => a * b);
+        }
+
+        public static int[,] DotProduct(int[,] a, int[,] b) {
+            if(a.GetLength(1) != b.GetLength(0)) throw new FormatException();
+            int rows = a.GetLength(0);
+            int columns = b.GetLength(1);
+            int[,] dotMatrix = new int[rows, columns];
+            for (int ay = 0; ay < rows; ay++) {
+                for (int bx = 0; bx < columns; bx++) {
+                    var column = GetColumn(b, bx);
+                    var product = 0;
+                    for (int i = 0; i < column.Length; i++)
+                        product += a[ay, i] * column[i]; 
+                    dotMatrix[ay, bx] = product;
+                }
+            }
+            return dotMatrix;
+        }
+
+        public static int[] GetColumn(int[,] matrix,int num) {
+            int length = matrix.GetLength(0);
+            int[] column = new int[length];
+            for (int y = 0; y < length; y++)
+                column[y] = matrix[y, num];
+            return column;
+        }
+
+        public static bool IsSameSize(int[,] a, int[,] b) {
+            return a.GetLength(0) == b.GetLength(0)
+                && a.GetLength(1) == b.GetLength(1);
         }
     }
 }
--- Hazi-Math/Program.cs
+++ Hazi-Math/Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
+using System.Linq;
 
 namespace Hazi_Math {
     class Program {
-        static readonly (string op, Func<int[,], int[,], int[,]> fun)[] operations = {
-            ("Összeadás", Matrix.Sum),
-            ("Kivonás", Matrix.Sub),
-            ("Skaláris Szorzás", Matrix.Scalar),
+        static readonly (
+            string op,
+            Func<int[,], int[,], int[,]> fun,
+            Func<int[,], int[,], bool> filter)[] operations = {
+            ("Összeadás", Matrix.Sum, Matrix.IsSameSize),
+            ("Kivonás", Matrix.Sub, Matrix.IsSameSize),
+            ("Skaláris Szorzás", Matrix.Scalar, Matrix.IsSameSize),
+            ("Mátrix Szorzat",Matrix.DotProduct,(a,b)=>a.GetLength(1) == b.GetLength(0))
         };
+
 
         static void Main(string[] args) {
             Console.WriteLine("Első mátrix: ");
@@ -14,35 +20,35 @@ namespace Hazi_Math {
             Console.WriteLine("\nMásodik mátrix: ");
             var b = ReadMatrix();
 
-            if (a.GetLength(0) != b.GetLength(0)
-                || a.GetLength(1) != b.GetLength(1)) {
-                Console.WriteLine("A mátrixok nem megegyező méretűek");
+            var filteredOptions = operations.Where(i => i.filter(a, b)).ToArray();
+            if (filteredOptions.Length == 0) {
+                Console.WriteLine("Ezeken a mátrixokon nem lehet operációkat elvégezni");
                 return;
             }
-
             Console.WriteLine("\nKérem válasszon egy Mátrix operációt: ");
-            for (int i = 0; i < operations.Length; i++) {
-                var (op, _) = operations[i];
-                Console.WriteLine($"\t {i+1}. {op}");
+            for (int i = 0; i < filteredOptions.Length; i++) {
+                var (op, _, _) = filteredOptions[i];
+                Console.WriteLine($"\t {i + 1}. {op}");
             }
             Console.Write("Válasz: ");
             var input = int.Parse(Console.ReadLine());
- 
-            var res = operations[input - 1].fun(a, b);
+
+            var res = filteredOptions[input - 1].fun(a, b);
             Console.WriteLine("\nKapott mátrix az operáció elvégzése után:");
             DrawMatrix(res);
         }
 
+
         static int[,] ReadMatrix() {
-            Console.Write("Kérem a mátrix oszlopainak számát: ");
-            int height = int.Parse(Console.ReadLine());
             Console.Write("Kérem a mátrix sorainak számát: ");
+            int height = int.Parse(Console.ReadLine());
+            Console.Write("Kérem a mátrix oszlopainak számát: ");
             int width = int.Parse(Console.ReadLine());
 
             int[,] matrix = new int[height, width];
             for (int y = 0; y < height; y++) {
                 for (int x = 0; x < width; x++) {
-                    Console.Write($"Kérem a mátrix [i:{y+1},j:{x+1}] elemét: ");
+                    Console.Write($"Kérem a mátrix [i:{y + 1},j:{x + 1}] elemét: ");
                     matrix[y, x] = int.Parse(Console.ReadLine());
                 }
             }
@@ -53,7 +59,7 @@ namespace Hazi_Math {
             int height = matrix.GetLength(0), width = matrix.GetLength(1);
             for (int y = 0; y < height; y++) {
                 for (int x = 0; x < width; x++)
-                    Console.Write("{0}\t", matrix[x, y]);
+                    Console.Write("{0}\t", matrix[y, x]);
                 Console.WriteLine();
             }
         }
