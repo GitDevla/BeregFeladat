5f7f6aa0682168ec1e5461a32c7dd323e838f2bc
GitDevla <pataki.david.ferenc@gmail.com>
2023-02-06 12:23:53 +0100

    Create required classes

---
+++ CorvinMoziApp/Mozi.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+
+namespace CorvinMoziApp {
+    class Mozi {
+        public List<Terem> termek = new List<Terem>();
+
+        public void Mentes() {
+
+        }
+
+        public Mozi(string forras) {
+            using (var reader = new StreamReader(forras)) {
+                while (!reader.EndOfStream) {
+                    var currName = reader.ReadLine();
+                    var temp = reader.ReadLine().Split(';');
+                    var currHeight = int.Parse(temp[0]);
+                    var currWidth = int.Parse(temp[1]);
+                    var currSeats = new List<string>();
+                    var line = reader.ReadLine();
+                    do {
+                        currSeats.Add(line);
+                        line = reader.ReadLine();
+                    } while (!string.IsNullOrEmpty(line));
+
+                    char[,] seats = new char[currHeight, currHeight];
+                    foreach (var house in currSeats) {
+                        var temp2 = house.Split(';');
+                        int y = int.Parse(temp[0]), x = int.Parse(temp[1]);
+                        seats[y, x] = temp2[2][0];
+                    }
+
+                    var currImage = Image.FromFile($"{currName.Split(' ')[0]}.jpg");
+                    termek.Add(new Terem(currName, currHeight, currWidth, seats, currImage));
+                }
+            }
+        }
+    }
+}
---
+++ CorvinMoziApp/Terem.cs
@@ -0,0 +1,26 @@
+﻿using System.Drawing;
+
+namespace CorvinMoziApp {
+    class Terem {
+        private string nev;
+        private Image nevadokep;
+        private int sorok;
+        private int szekek;
+        private char[,] ulesek;
+
+        public string Nev { get => nev; set => nev = value; }
+        public Image Nevadokep { get => nevadokep; set => nevadokep = value; }
+        public int Sorok { get => sorok; set => sorok = value; }
+        public int Szekek { get => szekek; set => szekek = value; }
+        public char[,] Ulesek { get => ulesek; set => ulesek = value; }
+
+        public Terem(string nev, int sorok, int szekek, char[,] ulesek, Image nevadokep) {
+            Nev = nev;
+            Nevadokep = nevadokep;
+            Sorok = sorok;
+            Szekek = szekek;
+            Ulesek = ulesek;
+        }
+
+    }
+}
