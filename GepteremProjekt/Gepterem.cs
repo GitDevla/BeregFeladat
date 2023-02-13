6d972e37e6fb85db9c37ca700da5ae569b3bdb90
cisco <cisco@B101-17>
2023-02-13 13:30:38 +0100

    Add Gepterem class

---
+++ GepteremProjekt/Gepterem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GepteremProjekt
+{
+    internal class Gepterem
+    {
+        private int[,] ertekeles;
+        private int helyDb;
+        private string nev;
+        private int sorDb;
+
+        public Gepterem(string nev, int sorDb, int helyDb, int[,] ertekeles)
+        {
+            Ertekeles = ertekeles;
+            HelyDb = helyDb;
+            Nev = nev;
+            SorDb = sorDb;
+        }
+
+        public int[,] Ertekeles { get => ertekeles; set => ertekeles = value; }
+        public int HelyDb { get => helyDb; set => helyDb = value; }
+        public string Nev { get => nev; set => nev = value; }
+        public int SorDb { get => sorDb; set => sorDb = value; }
+    }
+}
